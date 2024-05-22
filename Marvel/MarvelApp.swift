b0b82ce5f7c21d69e02ab2ea152558a18f5d88b7
Aniket Patil <aniket.p@reapmind.com>
2024-05-22 13:18:22 +0530

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Aniket Patil on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
